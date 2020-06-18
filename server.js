fafa83688c56a344a913d8e0b5b01739777daf18
Daisy Johnson <daisyjane.johnson@gmail.com>
2020-06-17 19:47:15 -0600

    finsished feature 2

--- server.js
+++ server.js
@@ -16,61 +16,104 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
 
 //ROUTES
 app.get('/', getBooks);
 app.get('/books/:id', getOneBook);
-app.post('/addBook', addNewBook);
-app.post('/searches', getBooksFromAPI);
 app.get('/searches', searchBook);
+app.put('/update/:id', updateBook);
+app.delete('/delete/:id', deleteBook);
+app.post('/detail', addNewBook);
+app.post('/searches', getBooksFromAPI);
 
-function searchBook (request, response) {
+function getBookshelves(request, response) {
+  let sql = 'SELECT DISTINCT bookshelf FROM books;';
+  return client.query(sql);
+}
+
+function deleteBook(request, response) {
+  let bookId = request.params.id;
+  // let { title, author, isbn, image_url, description } = request.body;
+
+  let sql = 'DELETE FROM books WHERE id=$1;';
+  let safeValues = [bookId];
+
+  client.query(sql, safeValues)
+    .then(sqlResults => {
+      // console.log('my sql results', sqlResults.rows);
+      response.redirect(`/`);
+    }).catch(error => console.log(error));
+}
+
+
+function updateBook(request, response) {
+  let bookId = request.params.id;
+  let { title, author, isbn, image_url, description } = request.body;
+
+  let sql = 'UPDATE books SET title=$1, author=$2, isbn=$3, image_url=$4, description=$5 WHERE id=$6;';
+  let safeValues = [title, author, isbn, image_url, description, bookId];
+
+  client.query(sql, safeValues)
+    .then(sqlResults => {
+      // console.log('my sql results', sqlResults.rows);
+      response.redirect(`/books/${bookId}`);
+    }).catch(error => console.log(error));
+}
+
+function searchBook(request, response) {
   response.status(200).render('./searches/new.ejs');
 }
 
-function getOneBook(request, response){
+function getOneBook(request, response) {
   let id = request.params.id;
 
   let sql = 'SELECT * FROM books WHERE id=$1;';
   let safeValues = [id];
 
   client.query(sql, safeValues)
-    .then(sqlResults =>{
-      console.log('my sql results', sqlResults.rows);
-      response.status(200).render('pages/books/detail.ejs', {oneBook: sqlResults.rows[0]});
-    });
-}
-
-function getBooks(request, response) {
-  let sql = 'SELECT * FROM books;';
-  client.query(sql)
     .then(sqlResults => {
-      let books = sqlResults.rows;
-      // Count for number of books in database
-      // console.log(books.length);
-      if(sqlResults.rowCount === 0){
-        response.status(200).render('pages/searches/news.ejs');
-      } else{
-        response.status(200).render('pages/index.ejs', { myBookShelf: books })
-
-      }
+      // console.log('my sql results', sqlResults.rows);
+      response.status(200).render('pages/books/detail.ejs', { oneBook: sqlResults.rows[0] });
     }).catch(error => console.log(error))
 }
 
-function addNewBook(request,response){
-  let {title, author, isbn, image_url, description} = request.body;
-  let sql = 'INSERT INTO books (author, title, isbn, description, image_url) VALUES ($1, $2, $3, $4, $5) RETURNING ID;';
-  let safeValues = [author, title, isbn, description, image_url];
+function getBooks(request, response) {
+  getBookshelves()
+    .then(bookshelf => {
+      // console.log(bookshelf);
+      let sql = 'SELECT * FROM books;';
+      client.query(sql)
+        .then(sqlResults => {
+          let books = sqlResults.rows;
+          // Count for number of books in database
+          // console.log(books.length);
+          if (sqlResults.rowCount === 0) {
+            response.status(200).render('pages/searches/news.ejs');
+          } else {
+            response.status(200).render('pages/index.ejs', { myBookShelf: books, totalBookshelves: bookshelf.rows })
 
-  client.query(sql, safeValues)
-    .then(results =>{
-      let id = results.rows[0].id;
-      response.redirect(`/books/${id}`);
+          }
+        }).catch(error => console.log(error))
     })
 }
 
+function addNewBook(request, response) {
+  let { title, author, isbn, image_url, description, bookshelf } = request.body;
+  // console.log(bookshelf);
+  let sql = 'INSERT INTO books (author, title, isbn, description, image_url, bookshelf) VALUES ($1, $2, $3, $4, $5, $6) RETURNING ID;';
+  let safeValues = [author, title, isbn, description, image_url, bookshelf];
 
-function getBooksFromAPI (request, response){
+  client.query(sql, safeValues)
+    .then(results => {
+      let id = results.rows[0].id;
+      response.redirect(`/books/${id}`);
+    }).catch(error => console.log(error))
+}
+
+
+function getBooksFromAPI(request, response) {
   try {
     // console.log(request.body.search);
 
